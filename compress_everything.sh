ae5f61e9d1ef0288a6439b740d70a5c7c353bdf4
Gabe Johnson <gabe.johnson@gmail.com>
2025-02-11 11:38:31 -0700

    Update svgo to latest, configure it, recompress everything

---
+++ compress_everything.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -e
+
+# npm run compress:bain_pezu_sections
+# npm run compress:baramula_sections
+# npm run compress:basawa_sections
+# npm run compress:bhangala_sections
+# npm run compress:buttar_sections
+# npm run compress:chambal_sections
+# npm run compress:chandigar_sections
+# npm run compress:dag_sections
+# npm run compress:dina_sections
+
+# npm run compress:eastern_potwar_sections # This one is problematic
+
+# npm run compress:gabhir_sections
+# npm run compress:garhi_sections
+# npm run compress:guma_sections
+# npm run compress:hirpur_sections
+# npm run compress:hari_sections
+# npm run compress:index_pakistan
+# npm run compress:index_india
+
+# npm run compress:index # Problematic
+
+# npm run compress:indus
+# npm run compress:marwat_sections
+# npm run compress:western_india
+# npm run compress:east_india
+# npm run compress:jari_sections
+# npm run compress:kangra_sections
+# npm run compress:nalad_sections
+# npm run compress:kas_dovac_sections
+# npm run compress:kashmir
+# npm run compress:key_map_pakistan_sections
+# npm run compress:key_map_western_india_sections
+# npm run compress:key_map_eastern_india_sections
+# npm run compress:khaur_sections
+# npm run compress:khaur_riwat_sections
+# npm run compress:mawa_sections
+# npm run compress:mirpur_sections
+# npm run compress:north_rohtas_sections
+# npm run compress:pandori_sections
+# npm run compress:peshawar
+# npm run compress:pabbi_sections
+# npm run compress:pind_sections
+# npm run compress:potwar
+# npm run compress:potwar_east
+# npm run compress:time_map
+# npm run compress:rata_sections
+
+# npm run compress:regional # Problem
+
+# npm run compress:romushi_sections
+# npm run compress:sakrana_sections
+# npm run compress:sauj_sections
+# npm run compress:shahpur_sections
+# npm run compress:kameng_sections
+# npm run compress:muksar_sections
+# npm run compress:surai_sections
+# npm run compress:punjab_sections
+# npm run compress:tinau_sections
+# npm run compress:swat_sections
+# npm run compress:shaliganga_sections
+# npm run compress:soan
+
+# npm run compress:subregional # Problem
+
+# npm run compress:tatrot_sections
